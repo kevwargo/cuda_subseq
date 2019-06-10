2ad1fd218fb0844e3cc6e82975f9df9643bcfa4b
student <student@LAB-DBX-2>
2019-06-10 13:13:56 +0200

    junk2

--- subseq_tmpl/kernel.cu
+++ subseq_tmpl/kernel.cu
@@ -1,8 +1,12 @@
 
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
 
 #include <stdio.h>
+#include <string.h>
+#include <iostream>
 
 __device__ __host__ unsigned newton(unsigned n, unsigned k)
 {
@@ -21,13 +25,50 @@ __device__ __host__ unsigned newton(unsigned n, unsigned k)
 	return top / bottom;
 }
 
-__global__ void check_subseq(unsigned patlen, unsigned seqlen, int *pat, int *seq, unsigned n)
+__global__ void check_subseq(unsigned n, unsigned k, int *seq, int *pat, int *combinations, unsigned size)
 {
+	int idx = blockDim.x * blockIdx.x + threadIdx.x;
+	int data[k];
+
+	if (idx >= size) {
+		return;
+	}
+
 
 }
 
-int main()
+int main(int argc, char **argv)
 {
+	unsigned n, k, comb_count;
+	if (argc < 3) {
+		std::cerr << "Not enough args" << std::endl;
+		return 1;
+	}
+	n = strlen(argv[1]);
+	k = strlen(argv[2]);
 
+	thrust::host_vector<int> h_combinations;
+	thrust::device_vector<int> d_combinations;
+	thrust::host_vector<int> h_seq;
+	thrust::device_vector<int> d_seq;
+	thrust::host_vector<int> h_pat;
+	thrust::device_vector<int> d_pat;
+
+	for (int i = 0; i < n; i++) {
+		h_seq.push_back(argv[1][i]);
+	}
+	for (int i = 0; i < k; i++) {
+		h_pat.push_back(argv[2][i]);
+	}
+	d_seq = h_seq;
+	d_pat = h_pat;
+	comb_count = newton(n, k);
+	d_combinations.reserve(comb_count * k);
+
+	check_subseq << <1, comb_count >> > (n, k, d_seq.data().get(), d_pat.data().get(), d_combinations.data().get(), comb_count);
+
+	h_combinations = d_combinations;
+
+	return 0;
 }
 
